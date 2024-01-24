811194bfca20b38884f372e6f49a841be7c8dcd6
Guada <125298307+GuadaGT@users.noreply.github.com>
2024-01-24 09:39:14 +0100

    MAP y FILTER mostrar por pantalla a través de un array

--- src/app/components/operadores/operadores.component.ts
+++ src/app/components/operadores/operadores.component.ts
@@ -10,24 +10,45 @@ export class OperadoresComponent
 {
   numbers1 = of(1,2,3);
   numbers2= of(4,5,6);
+
+  //Calcula cuadrados
   squaredNumbers = this.numbers1.pipe(map(x => x * x));
-  evenNumbers = this.numbers2.pipe(filter(x => x % 2 === 0));
-  reducedNumbers = this.numbers2.pipe(reduce((x,y) => x+y));
-  mergedNumbers = merge(this.numbers1,this.numbers2);
-  concatNumbers = concat(this.numbers1,this.numbers2);
+  //Sacar números pares
+  evenNumbers= this.numbers2.pipe(filter(x=> x % 2 === 0));
+
+  coleccionMap: number [] =[];
+  coleccionFilter: number [] = [];
 
   constructor()
   {
     this.operadorMap();
+    this.operadorFilter();
   }
 
   operadorMap()
   {
     this.squaredNumbers.subscribe(
       {
-        next: res=> console.log('Map',res),
+        next: res=>{
+            console.log('Map',res),
+              this.coleccionMap.push(res)
+        },
         error:err=> console.error(err),
         complete:()=> console.log('Map Complete')
+      }
+    )
+  }
+
+  private operadorFilter()
+  {
+    this.evenNumbers.subscribe(
+      {
+        next: res=> {
+          console.log('Filter', res),
+            this.coleccionFilter.push(res)
+        },
+        error:err=> console.error(err),
+        complete:()=>console.log('Filter complete')
       }
     )
   }
