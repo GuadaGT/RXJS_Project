4cb546fb55201191aa90a9aaa2466a64aba05c3d
Guada <125298307+GuadaGT@users.noreply.github.com>
2024-01-26 13:39:10 +0100

    Behavior subject

--- src/app/components/behavior-subject/behavior-subject.component.ts
+++ src/app/components/behavior-subject/behavior-subject.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import {BehaviorSubject} from "rxjs";
+import {RxjsService} from "../../services/rxjs.service";
 
 @Component({
   selector: 'app-behavior-subject',
   templateUrl: './behavior-subject.component.html',
   styleUrls: ['./behavior-subject.component.css']
 })
-export class BehaviorSubjectComponent {
+export class BehaviorSubjectComponent
+{
+    myBehaviorSubject = new BehaviorSubject<String>('Inicial');
 
+    constructor(private service: RxjsService){}
+    ejemploBS()
+    {
+      this.myBehaviorSubject.next('Holiiiii!');
+      this.service.addElement(1);
+    }
 }
